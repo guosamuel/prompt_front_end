338b0cc77666b63f3efbafc16676ad61e9f72971
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-10-18 15:23:50 -0400

    separate auth token file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { authToken } from './authToken/authToken.js'
 
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    allUniversities: []
+  }
+
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/api/v1/profile', {
+  //     method: 'GET',
+  //     headers: {
+  //       Authorization: `Token <token>`
+  //     }
+  //   })
+  // }
+
+  render() {
+    console.log(authToken)
+    return (
+      <div className="App">
+        Hello World!
+      </div>
+    );
+  }
 }
+
 
 export default App;
