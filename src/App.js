2223f46c1a6b7ea7e17d9cb9ee0db16a547cc28e
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-10-19 16:38:31 -0400

    got semantic ui react working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { DropDown } from 'semantic-ui-react'
+import { Dropdown } from 'semantic-ui-react'
+import TitleBar from './components/TitleBar'
+import { universities } from './universities/universities'
 
 class App extends React.Component {
 
@@ -10,11 +12,18 @@ class App extends React.Component {
   }
 
   render() {
+
+    const universityOptions = universities.map( (university, index) => ({
+      key: index,
+      text: university.name,
+      value: index,
+    }))
+
     return (
       <div>
         <TitleBar currentUniversity={this.state.selectedUniversity}/>
         <label>Please select your university</label>
-
+        <Dropdown placeholder="Please Select A University" search selection options={universityOptions}/>
       </div>
     );
   }
--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export default function TitleBar(props) {
+  console.log(props)
   return (
     <div>
-      {props.currentUniversity ? `Essay Requirements` : `Essay Requirements - ${props.name}}
+      {props.currentUniversity ? `Essay Requirements` : `Essay Requirements - DERP`}
     </div>
   )
 }
