21bce322fff8ed6172eca62c81f5b6ce7cfbde94
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-10-21 15:51:49 -0400

    added minimum and maximum words

--- src/components/PromptCard.js
+++ src/components/PromptCard.js
@@ -1,5 +1,15 @@
 import React from 'react'
 
 export default function PromptCard(props) {
-  return "FILL OUT THE INFORMATION NEXT TIME!"
+  // console.log("IM IN THE PROMPT CARD", props)
+  return (
+    <div id={props.prompt.slug}>
+
+      <label>Minimum Length: </label>
+      { props.prompt.min_length ? `${props.prompt.min_length.value} words` : "1 word"}
+      <br />
+      <label>Maximum Length: </label>
+      { props.prompt.max_length ? `${props.prompt.max_length.value} words` : "800 words"}
+    </div>
+  )
 }
