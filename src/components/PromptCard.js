10cc5e41709f3da66b21ab9cf2c7aa2d7f400aad
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-10-21 17:28:19 -0400

    next to is pass the slug up to the state where all the prompts live

--- src/components/PromptCard.js
+++ src/components/PromptCard.js
@@ -1,24 +1,57 @@
 import React from 'react'
 
-export default function PromptCard(props) {
-  // console.log("IM IN THE PROMPT CARD", props)
-  return (
-    <div id={props.prompt.slug}>
-      <label>Prompt Title: </label>
-      { props.prompt.topic }
-      <br />
-      <br />
-      <label>Description: </label>
-      { props.prompt.prompt }
-      <br />
-      <br />
-      <label>Minimum Length: </label>
-      { props.prompt.min_length ? `${props.prompt.min_length.value} words` : "1 word"}
-      <br />
-      <label>Maximum Length: </label>
-      { props.prompt.max_length ? `${props.prompt.max_length.value} words` : "800 words"}
-      <br />
-      <br />
-    </div>
-  )
+export default class PromptCard extends React.Component {
+
+  state = {
+    description: this.props.prompt.prompt
+  }
+
+  handleDescriptionChange = (e) => {
+    this.setState({description: e.target.value})
+  }
+
+  handleDescriptionSubmit = (e, slug) => {
+    e.preventDefault()
+    console.log(slug)
+  }
+
+  render() {
+    console.log(this.state.description)
+    return (
+      <div id={this.props.prompt.slug}>
+        <label>Prompt Title: </label>
+        { this.props.prompt.topic }
+
+        <br />
+        <br />
+
+        <label>Prompt: </label>
+        <form onSubmit={(e, slug) => this.handleDescriptionSubmit(e, this.props.prompt.slug)}>
+          <textarea
+            rows='5'
+            cols='100'
+            name='description'
+            value={this.state.description}
+            onChange={this.handleDescriptionChange}>
+          </textarea>
+          <br />
+          <button type="submit">Submit Edits</button>
+        </form>
+
+        <br />
+        <br />
+
+        <label>Minimum Length: </label>
+        { this.props.prompt.min_length ? `${this.props.prompt.min_length.value} words` : "1 word"}
+
+        <br />
+
+        <label>Maximum Length: </label>
+        { this.props.prompt.max_length ? `${this.props.prompt.max_length.value} words` : "800 words"}
+
+        <br />
+        <br />
+      </div>
+    )
+  }
 }
